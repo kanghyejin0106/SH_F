9ca4d63ba58923294de7a7eaf41b06db3ceaa1cf
sonhyejin <sjh8205@naver.com>
2018-05-21 21:15:34 +0900

    d

--- app/src/main/java/com/example/home/myapplication/Zero.java
+++ app/src/main/java/com/example/home/myapplication/Zero.java
@@ -1,9 +1,11 @@
 package com.example.home.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +19,11 @@ public class Zero extends AppCompatActivity {
     String name_s, pw_s, phone_s,email_s;
     EditText name, pw, phone,email;
     TextView text;
+    boolean status = true;
+    Button female;
+    Button male;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,77 @@ public class Zero extends AppCompatActivity {
 
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitDiskReads().permitDiskWrites().permitNetwork().build());
 
+        final boolean Status[]={true, false};
+
+        female = (Button)findViewById(R.id.Female);
+        male = (Button)findViewById(R.id.Male);
+        /*
+
+        female.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        female.setBackgroundColor(R.color.Clicked);
+                        //male.setBackgroundColor(R.color.Male);
+                        v.setPressed(true);
+                        }
+                    case MotionEvent.ACTION_UP:{
+                        female.setBackgroundColor(R.color.Female);
+                        //male.setBackgroundColor(R.color.Clicked);
+                        v.setPressed(false);
+                        break;}
+                }
+                return false;
+            }
+        });
+
+        male.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:{
+                        male.setBackgroundColor(R.color.Clicked);
+                        //female.setBackgroundColor(R.color.Female);
+                        v.setPressed(true);
+                        break;}
+                    case MotionEvent.ACTION_UP:{
+                        //female.setBackgroundColor(R.color.Clicked);
+                        male.setBackgroundColor(R.color.Male);
+                        v.setPressed(false);
+                        break;}
+                }
+                return false;
+            }
+        });*/
+
+
+        female.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                status = true;
+                if(status==true) {
+                    female.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.Clicked));
+                    female.setTextColor(getApplicationContext().getResources().getColor(R.color.White));
+                    male.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.Male));
+                    male.setTextColor(getApplicationContext().getResources().getColor(R.color.Black));
+                }
+            }
+        });
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                status = false;
+                if(status==false) {
+                    male.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.Clicked));
+                    male.setTextColor(getApplicationContext().getResources().getColor(R.color.White));
+                    female.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.Male));
+                    female.setTextColor(getApplicationContext().getResources().getColor(R.color.Black));
+                }
+            }
+        });
 
         name = (EditText)findViewById(R.id.Name);
 
@@ -59,11 +136,7 @@ public class Zero extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
-
                 Intent intent = new Intent(Zero.this, First.class);
-                intent.putExtra("Userid",name_s);
-                intent.putExtra("Userpw",pw_s);
-                intent.putExtra("Userph",phone_s);
                 startActivity(intent);
             }
         });
