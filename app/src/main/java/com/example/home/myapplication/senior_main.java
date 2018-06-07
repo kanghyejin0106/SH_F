bf3b71db8b67018abe5b79a116137925bc79afc9
kanghyejin0106 <34128604+kanghyejin0106@users.noreply.github.com>
2018-06-07 21:42:46 +0900

    회원가입

--- app/src/main/java/com/example/home/myapplication/senior_main.java
+++ app/src/main/java/com/example/home/myapplication/senior_main.java
@@ -1,7 +1,10 @@
 package com.example.home.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class senior_main extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class senior_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_senior_main);
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(senior_main.this,Senior_addroom.class);
+                startActivity(intent);
+            }
+        });
     }
 }
