66ae82fde303bc4bc9680684d91de4a41bf20570
Kangzingu <kanrhaehfdl1@naver.com>
2018-06-11 20:16:45 +0900

    이상한거 추가함
    맵 액티비티에 import android.net.Uri;

--- app/src/main/java/com/example/home/myapplication/map.java
+++ app/src/main/java/com/example/home/myapplication/map.java
@@ -4,7 +4,6 @@ import android.app.Fragment;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
